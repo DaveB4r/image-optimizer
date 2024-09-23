9ccf59883fc57c571854e50bc5a8475ef7e71127
DaveB4r <davidpinedaaaa@gmail.com>
2024-09-23 00:17:14 -0500

    filename updated

--- sharp.js
+++ sharp.js
@@ -4,10 +4,11 @@ const fs = require('fs');
 const files = fs.readdirSync('./images');
 
 files.forEach((file, i) => {
+  const fileName = `${String(file).replace(/(.png|.jpg|.jpeg)/, '')}`;
   sharp(`images/${file}`)
     // .resize(600,500)
     .webp({quality: 80})
-    .toFile(`optimized/${i+371}.webp`, (err, info) => {
+    .toFile(`optimized/${fileName}.webp`, (err, info) => {
       if (err) {
         console.error(err);
       } else {
